6c5f4ab78b3efb12d5943a2a25260ed34d05bdbd
Dmitry Kontsevoy <dmitry.kontsevoy@gmail.com>
2016-07-23 23:14:51 +0300

    remove old files

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build --force-rm=true -t h3xby/ruby ruby
-docker build --force-rm=true -t h3xby/rails rails/default
-docker build --force-rm=true -t h3xby/rails:onbuild rails/onbuild
-
--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker push h3xby/ruby
-docker push h3xby/rails
