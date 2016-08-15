47a6db1aa466df168a6a72a287f28c94db9e8a2a
Dmitry Kontsevoy <dmitry.kontsevoy@gmail.com>
2016-08-15 20:38:49 +0300

    haproxy-proxylist: add url for downloading txt

--- haproxy-proxylist/proxy-list.sh
+++ haproxy-proxylist/proxy-list.sh
@@ -1,40 +1,8 @@
 #!/bin/bash
 set -e
 
-HAPROXY_CONFIG="/usr/local/etc/haproxy/haproxy.cfg"
-
-cat >$HAPROXY_CONFIG <<TXT 
-global
-    maxconn 2000
-    # log /dev/stdout local0 info
-
-defaults
-    log     global
-    mode    tcp
-    option tcplog
-    option  dontlognull
-    option redispatch
-
-    retries 3
-    timeout connect  5000
-    timeout client  10000
-    timeout server  10000
-
-listen proxy
-    bind 0.0.0.0:8118
-    use_backend proxies
-
-backend proxies
-    balance roundrobin
-TXT
-
-num=0
-while read i
-do
-  let "num += 1"
-  echo "    server app${num} ${i}" >>$HAPROXY_CONFIG
-done <$PROXY_FILE
+/proxyfile-generate.sh
+cron
 
 set -- /docker-entrypoint.sh "$@"
-echo $@
 exec "$@"
---
+++ haproxy-proxylist/proxyfile-generate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+HAPROXY_CONFIG="/usr/local/etc/haproxy/haproxy.cfg"
+HAPROXY_PIDFILE="/run/haproxy.pid"
+
+cat >$HAPROXY_CONFIG <<TXT 
+global
+    maxconn 2000
+    # log /dev/stdout local0 info
+
+defaults
+    log     global
+    mode    tcp
+    option tcplog
+    option  dontlognull
+    option redispatch
+
+    retries 3
+    timeout connect  5000
+    timeout client  10000
+    timeout server  10000
+
+listen proxy
+    bind 0.0.0.0:8118
+    use_backend proxies
+
+backend proxies
+    balance roundrobin
+TXT
+
+if [ -n "$PROXY_FILE_URL" ]
+then
+  PROXY_FILE=/tmp/proxyfile.txt
+  curl $PROXY_FILE_URL > $PROXY_FILE
+fi
+
+num=0
+while read i
+do
+  let "num += 1"
+  echo "    server app${num} ${i}" >>$HAPROXY_CONFIG
+done <$PROXY_FILE
+
+if [ -f $HAPROXY_PIDFILE ]
+then
+  kill -s SIGHUP `cat $HAPROXY_PIDFILE`
+fi
