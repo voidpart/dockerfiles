bca3beabf6c4984bdd58fc1f634f4a89e03511ab
Dmitry Kontsevoy <dmitry.kontsevoy@gmail.com>
2017-01-09 10:57:38 +0300

    ruby: 2.4 & alpine versions

--- ruby/update.sh
+++ ruby/update.sh
@@ -5,11 +5,14 @@ versions=(
   '2.1' 
   '2.2' 
   '2.3'
+  '2.4'
 )
 
 for version in ${versions[*]}; do
   echo Templating $version
   sedStr="s!%%VERSION%%!$version!g;"
 
+  mkdir -p $version $version-alpine
   sed -r "$sedStr" "Dockerfile.template" > "$version/Dockerfile"
+  sed -r "$sedStr" "Dockerfile-alpine.template" > "${version}-alpine/Dockerfile"
 done
