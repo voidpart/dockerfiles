9326148d233702e347bc2fee58b9713311abc555
Dmitry K <dmitry@yep.by>
2017-11-02 10:52:33 +0300

    ruby: rollback to alpine3.4 for 2.1-2.3

--- ruby/update.sh
+++ ruby/update.sh
@@ -12,7 +12,24 @@ for version in ${versions[*]}; do
   echo Templating $version
   sedStr="s!%%VERSION%%!$version!g;"
 
-  mkdir -p $version $version-alpine
+  mkdir -p $version
   sed -r "$sedStr" "Dockerfile.template" > "$version/Dockerfile"
+done
+
+versions=(
+  '2.1' 
+  '2.2' 
+  '2.3'
+)
+for version in ${versions[*]}; do
+  mkdir -p $version-alpine
   sed -r "$sedStr" "Dockerfile-alpine.template" > "${version}-alpine/Dockerfile"
+done
+
+versions=(
+  '2.4' 
+)
+for version in ${versions[*]}; do
+  mkdir -p $version-alpine
+  sed -r "$sedStr" "Dockerfile-alpine3.6.template" > "${version}-alpine/Dockerfile"
 done
