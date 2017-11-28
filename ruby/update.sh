d10fe07aa718f2ac15d1f779a99b96eb020131ed
Dmitry K <dmitry@yep.by>
2017-11-28 15:11:09 +0300

    ruby: differenciate alpine3.4 & alpine3.6

--- ruby/update.sh
+++ ruby/update.sh
@@ -20,16 +20,23 @@ versions=(
   '2.1' 
   '2.2' 
   '2.3'
+  '2.4'
 )
 for version in ${versions[*]}; do
+  echo Templating $version-alpine
+  sedStr="s!%%VERSION%%!$version!g;"
+
   mkdir -p $version-alpine
   sed -r "$sedStr" "Dockerfile-alpine.template" > "${version}-alpine/Dockerfile"
 done
 
 versions=(
-  '2.4' 
+  '2.4'
 )
 for version in ${versions[*]}; do
-  mkdir -p $version-alpine
-  sed -r "$sedStr" "Dockerfile-alpine3.6.template" > "${version}-alpine/Dockerfile"
+  echo Templating $version-alpine3.6
+  sedStr="s!%%VERSION%%!$version!g;"
+
+  mkdir -p $version-alpine3.6
+  sed -r "$sedStr" "Dockerfile-alpine3.6.template" > "${version}-alpine3.6/Dockerfile"
 done
